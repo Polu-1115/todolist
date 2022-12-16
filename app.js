40b5347d81ddb86a3315533c1d16e07b02d20ee4
Pralay Mondal <poluchamp01@gmail.com>
2022-12-16 21:01:32 +0530

    Completed the TodoList with multiple pages and layouts, Also refactored the codes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,28 +16,36 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
+  const day = date.getDate();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-us", options);
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 
 app.post("/", function(req, res){
-  let item = req.body.newItem;
 
-  items.push(item);
+  const item = req.body.newItem;
 
-  res.redirect("/");
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
+
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
 
 
 app.listen(3000, function() {
