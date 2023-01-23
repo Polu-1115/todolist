cee850d9dd356290b1c49ef895de3d3950997deb
Pralay Mondal <poluchamp01@gmail.com>
2023-01-23 21:21:51 +0530

    setting up for heroku

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ app.get("/about", function(req, res) {
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT, function() {
+  console.log("Server started on port successfully.");
 });
